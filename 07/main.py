fb368723ffda14d220a8bcad210ac2e5ae8555ab
Antoine Poullet <antoine@poullet.info>
2023-12-07 09:08:18 +0000

    Small cleanup of day 7

--- 07/main.py
+++ 07/main.py
@@ -1,9 +1,16 @@
 import sys
 
-from functools import cmp_to_key
+from functools import cmp_to_key, partial, reduce
+
 
 with open(sys.argv[1], 'r') as f:
     lines = list(map(str.strip,f.readlines()))
+
+hands_to_bids = { hand: int(bid) for hand, bid in map(lambda l: l.split(' '), lines) }
+
+def get_bid(hand_with_rank):
+    hand = hand_with_rank[0]
+    return hands_to_bids[hand]
 
 card_ranks = {
     'A': 0,
@@ -21,14 +28,16 @@ card_ranks = {
     '2': 12,
 }
 
+card_ranks_j = { **card_ranks, 'J': 13 }
+
 hand_ranks = {
-    '5': 0,
-    '41': 1,
-    '32': 2,
-    '311': 3,
-    '221': 4,
-    '2111': 5,
-    '11111': 6
+    5: 0,
+    41: 1,
+    32: 2,
+    311: 3,
+    221: 4,
+    2111: 5,
+    11111: 6
 }
 
 def get_hand_with_type(hand):
@@ -39,10 +48,10 @@ def get_hand_with_type(hand):
         else:
             index[card] = 1
 
-    hand_str = ''.join(sorted(''.join(map(str,index.values())), reverse=True))
-    return (hand, hand_ranks[hand_str])
+    key = reduce(lambda a, c: a * 10 + c, sorted(index.values(), reverse=True))
+    return (hand, hand_ranks[key])
 
-def compare_hands(fst, snd):
+def compare_hands_inner(card_ranks, fst, snd):
     if fst[1] < snd[1]:
         return 1
     elif fst[1] > snd[1]:
@@ -55,34 +64,13 @@ def compare_hands(fst, snd):
                 return -1
         return 0
 
-hands_to_bids = { hand: int(bid) for hand, bid in map(lambda l: l.split(' '), lines) }
+compare_hands = partial(compare_hands_inner, card_ranks)
 
-hands_with_types = list(map(get_hand_with_type, hands_to_bids.keys()))
+sorted_bids = map(get_bid, sorted(map(get_hand_with_type, hands_to_bids.keys()), key=cmp_to_key(compare_hands)))
 
-hands_with_types.sort(key=cmp_to_key(compare_hands))
+part_1 = reduce(lambda a, c: a + c[0] * c[1], enumerate(sorted_bids, 1), 0)
 
-total = 0
-for rank, (hand, _) in enumerate(hands_with_types, 1):
-    total += rank * hands_to_bids[hand]
-
-print(total)
-
-card_ranks_j = {
-    'A': 0,
-    'K': 1,
-    'Q': 2,
-    'J': 3,
-    'T': 4,
-    '9': 5,
-    '8': 6,
-    '7': 7,
-    '6': 8,
-    '5': 9,
-    '4': 10,
-    '3': 11,
-    '2': 12,
-    'J': 13,
-}
+print(part_1)
 
 def get_hand_with_type_j(hand):
     index = {}
@@ -103,27 +91,13 @@ def get_hand_with_type_j(hand):
     sorted_hand = sorted(index.values(), reverse=True)
     sorted_hand[0] += j_value
 
-    hand_str = ''.join(map(str, sorted_hand))
-    return (hand, hand_ranks[hand_str])
+    key = reduce(lambda a, c: a * 10 + c, sorted_hand)
+    return (hand, hand_ranks[key])
 
-def compare_hands_j(fst, snd):
-    if fst[1] < snd[1]:
-        return 1
-    elif fst[1] > snd[1]:
-        return -1
-    else:
-        for f, s in zip(fst[0], snd[0]):
-            if card_ranks_j[f] < card_ranks_j[s]:
-                return 1
-            elif card_ranks_j[f] > card_ranks_j[s]:
-                return -1
-        return 0
+compare_hands_j = partial(compare_hands_inner, card_ranks_j)
 
-hands_with_types_j = list(map(get_hand_with_type_j, hands_to_bids.keys()))
-hands_with_types_j.sort(key=cmp_to_key(compare_hands_j))
+sorted_bids_j = map(get_bid, sorted(map(get_hand_with_type_j, hands_to_bids.keys()), key=cmp_to_key(compare_hands_j)))
 
-total_j = 0
-for rank, (hand, _) in enumerate(hands_with_types_j, 1):
-    total_j += rank * hands_to_bids[hand]
+part_2 = reduce(lambda a, c: a + c[0] * c[1], enumerate(sorted_bids_j, 1), 0)
 
-print(total_j)
+print(part_2)
