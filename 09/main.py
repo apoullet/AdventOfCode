74c756e233e875804d5f3ae8f7b3676c16cc2bee
Antoine Poullet <antoine@poullet.info>
2023-12-09 07:32:10 +0000

    Clean up day 9
    Didn't spot this because I'm an idiot, thanks u/ThreadsOfCode

--- 09/main.py
+++ 09/main.py
@@ -21,19 +21,4 @@ def find_next_value(sequence):
     return sub_sequences[0][-1]
 
 print(sum(map(find_next_value, sequences)))
-
-def find_previous_value(sequence):
-    sub_sequences = [list(sequence)]
-
-    while sum(sub_sequences[-1]) != 0:
-        last_sequence = sub_sequences[-1]
-        new_sequence = list(map(lambda p: p[1]-p[0], zip(last_sequence[:-1], last_sequence[1:])))
-        sub_sequences.append(new_sequence)
-
-    for i, _ in enumerate(sub_sequences[:-1][::-1]):
-        previous = sub_sequences[-2-i+1][0]
-        sub_sequences[-2-i].insert(0, sub_sequences[-2-i][0] - previous)
-    
-    return sub_sequences[0][0]
-
-print(sum(map(find_previous_value, sequences)))
+print(sum(map(find_next_value, map(lambda s: s[::-1],sequences))))
